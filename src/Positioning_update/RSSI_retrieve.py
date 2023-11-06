ba446b0853378c0adde56c14f707a499c7054758
nguyennam2010 <102983698+nguyennam2010@users.noreply.github.com>
2023-11-06 18:13:07 +0800

    Update RSSI_retrieve.py

--- src/Positioning_update/RSSI_retrieve.py
+++ src/Positioning_update/RSSI_retrieve.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov  2 22:16:56 2023
-
-@author: Hoai-Nam Nguyen
+RSSI Main Script
+Author: Hoai-Nam Nguyen
 """
 
 import RSSI_function
@@ -13,71 +12,80 @@ import json
 import warnings
 from pymongo import MongoClient
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 warnings.filterwarnings("ignore")
-import json
 
 
 def main():
+    """
+    Main function to retrieve and store RSSI data from vMM to MongoDB.
+    """
     # Load variables from .env file
     load_dotenv()
-    
+
     # Login
     username = os.getenv("ACCOUNT")
     password = os.getenv("PASSWORD")
     vMM_aosip = os.getenv('vMM_aosip')
-    
-    # Building name 
+
+    # Building name
     building = 'IY'
-    
-    # Read the JSON file
+
+    # Read the JSON file containing AP data
     with open('AP_dict', 'r') as file:
         ref_aps_dict_all = json.load(file)
-        
+
     # Set RSSI threshold
-    threshold = 60 
+    threshold = 60
     # Max AP number per floor in the building. Take IY building as an example, max AP = 5.
     max_ap_per_floor = 5
 
-    #Get the token to access vMM information  -- via API
-    token = RSSI_function.authentication(username,password,vMM_aosip)
-    
+    # Get the token to access vMM information  -- via API
+    token = RSSI_function.authentication(username, password, vMM_aosip)
+
     # Get floor list
     f_name = RSSI_function.get_floor_list(ref_aps_dict_all)
-    
+
     # Get AP list
     AP_name = RSSI_function.get_AP_name(ref_aps_dict_all)
 
     # Get AP coordinates
     AP_coords = RSSI_function.get_AP_coords(ref_aps_dict_all, max_ap_per_floor)
-    
+
     # Get datetime
     ts, ts_tw_str, ts_tw = RSSI_function.calculate_taiwan_time()
-    
+
+    # Retrieve RSSI for reference APs in every floor
+    ref = RSSI_function.ref_retrieve_all(AP_name, f_name, vMM_aosip, token, building)
+
     # Retrieve RSSI for rogue APs in every floor
-    rogue = RSSI_function.rogue_rss_retrieve(AP_name[0], f_name[0], vMM_aosip, token, building, threshold)
-    
+    rogue = RSSI_function.rogue_retrieve_all(AP_name, f_name, vMM_aosip, token, building, threshold)
+
+    # Merge reference APs and rogue APs
+    dfa = ref.append(rogue)
+
     # Save as json format
-    data_json = json.loads(rogue.to_json(orient='records'))
-    
+    data_json = json.loads(dfa.to_json(orient='records'))
+
     # Add datetime
     for i in range(len(data_json)):
         data_json[i].update({'ts': ts, 'DatetimeStr': ts_tw_str, 'Datetime': ts_tw})
-    
-    # =============================================================================
-#     # Databases info
+
+    # Databases info
     MONGOIP = os.getenv('MONGOIP')
     DB = os.getenv('DB')
     COLLECTION = os.getenv('COLLECTION')
-    
-#     # Store json data to MongoDB
-#     client = MongoClient(MONGOIP,27017)
-#     db = client[DB]
-#     col=db[COLLECTION]
-#     col.insert_many(data_json)
-        
+
+    ## Store json data to MongoDB
+    # client = MongoClient(MONGOIP, 27017)
+    # db = client[DB]
+    # col = db[COLLECTION]
+    # col.insert_many(data_json)
+
     print('Done!')
-    print(rogue)
-    
+    print(dfa)
+
+
 if __name__ == "__main__":
     main()
