9dd3266c55e7a89a5b8052b3ec213eea645b12a5
nguyennam2010 <102983698+nguyennam2010@users.noreply.github.com>
2023-11-06 18:52:48 +0800

    Update RSSI_retrieve.py

--- src/Positioning_update/RSSI_retrieve.py
+++ src/Positioning_update/RSSI_retrieve.py
@@ -30,7 +30,7 @@ def main():
     vMM_aosip = os.getenv('vMM_aosip')
 
     # Building name
-    building = os.getenv('building')
+    building = os.getenv('BUILDING')
 
     # Read the JSON file containing AP data
     with open('AP_dict', 'r') as file:
